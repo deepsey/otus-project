4d73edf0fbda73257ec64d4e689cc1fcbd576980
deepsey <78343716+deepsey@users.noreply.github.com>
2021-08-15 21:12:31 +0300

    Add files via upload

---
+++ roles/web/files/script-backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Server name
+
+BORG_SERVER=project-backup
+
+# Backup type, it may be data, system, mysql, binlogs, etc.
+
+TYPE_OF_BACKUP=system
+
+REPOSITORY="${BORG_SERVER}:/var/backup/$(hostname)-${TYPE_OF_BACKUP}"
+
+#Create backup
+
+borg create --list -v --stats \
+ $REPOSITORY::"etc-{now:%Y-%m-%d-%H-%M}" \
+ /                                       \
+ --exclude '/proc/*'                     \
+ --exclude '/mnt/*'                      \
+ --exclude '/sys/*'                      \
+ --exclude '/media/*'                    \
+ --exclude '/dev/*' 
+ 
+ 
+
+ 
+#Prune old backup
+
+borg prune -v --list \
+    --keep-within=7d \
+    --keep-weekly=4 \
+    $REPOSITORY
